b652a8bd625c51d0acd9913cf1a39383b8e8c2d9
JPDBrodeur <jbrodeur001@gmail.com>
2021-10-17 07:56:32 -0400

    initial inquirer prompt added

---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer');
+const figlet = require('figlet');
+
+
+const promptUser = () => {
+    console.log(figlet.textSync(`Employee  
+Manager`));
+
+    inquirer.prompt({
+        type: 'list',
+        name: 'mainMenu',
+        message: 'What would you like to do?',
+        choices: ['View All Departments',
+        'View All Roles',
+        'View All Employees',
+        'Add Department',
+        'Add Role',
+        'Add Employee',
+        'Update Employee Role',
+        // 'Update Employee Manager',
+        // 'View All Employees By Manager',
+        // 'View All Employees By Department',
+        // 'Remove Department',
+        // 'Remove Role',
+        // 'Remove Employee',
+        // 'View Total Utilized Budget of Department',
+    ]
+    })
+    .then(({ mainMenu }) => {
+        switch (mainMenu) {
+            case 'View All Departments':
+                break;
+            case 'View All Roles':
+                break;
+            case 'View All Employees':
+                break;
+            case 'Add Department':
+                break;
+            case 'Add Role':
+                break;
+            case 'Add Employee':
+                break;
+            case 'Update Employee Role':
+                break;
+        }
+    });
+};
+
+promptUser();

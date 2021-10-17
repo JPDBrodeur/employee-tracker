1a6d67c26c8d48ad961474f5a88c79c3f0845d9c
JPDBrodeur <jbrodeur001@gmail.com>
2021-10-17 06:38:24 -0400

    db seeds from GIF

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO departments (name)
+VALUES 
+    ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Accountant', 125000, 2),
+    ('Lawyer', 190000, 3),
+    ('Lead Engineer', 150000, 1),
+    ('Legal Team Lead', 250000, 3),
+    ('Sales Lead', 100000, 4),
+    ('Salesperson', 80000, 4),
+    ('Software Engineer', 120000, 1);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+    ('John', 'Doe', 5, 3),
+    ('Mike', 'Chan', 6, 1),
+    ('Ashley', 'Rodriguez', 3, null),
+    ('Kevin', 'Tupik', 7, 3),
+    ('Malia', 'Brown', 1, null),
+    ('Sarah', 'Lourd', 4, null),
+    ('Tom', 'Allen', 2, 6),
+    ('Christian' 'Eckenrode', 3, 2);

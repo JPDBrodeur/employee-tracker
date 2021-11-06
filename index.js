ba57a110e55f2879ded2573b4a6497437ce638c9
JPDBrodeur <jbrodeur001@gmail.com>
2021-11-06 14:40:53 -0400

    EmployeesTable extension, pushed for troubleshooting with AskBCS

--- index.js
+++ index.js
@@ -22,32 +22,36 @@ const promptUser = () => {
         name: 'mainMenu',
         message: 'What would you like to do?',
         choices: [
-        'View All Departments',
-        'View All Roles',
-        'View All Employees',
-        'Add Department',
-        'Add Role',
-        'Add Employee',
-        'Update Employee Role',
-        // 'Update Employee Manager',
-        // 'View All Employees By Manager',
-        // 'View All Employees By Department',
-        // 'Remove Department',
-        // 'Remove Role',
-        // 'Remove Employee',
-        // 'View Total Utilized Budget of Department'
-    ]
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add Department',
+            'Add Role',
+            'Add Employee',
+            'Update Employee Role',
+            'Quit Application'
+            // 'Update Employee Manager',
+            // 'View All Employees By Manager',
+            // 'View All Employees By Department',
+            // 'Remove Department',
+            // 'Remove Role',
+            // 'Remove Employee',
+            // 'View Total Utilized Budget of Department'
+        ]
     })
     .then(({ mainMenu }) => {
         switch (mainMenu) {
             case 'View All Departments':
-                departments.viewAll();
+                departments.viewAll()
+                    .then(() => promptUser());
                 break;
             case 'View All Roles':
-                roles.viewAll();
+                roles.viewAll()
+                    .then(() => promptUser());
                 break;
             case 'View All Employees':
-                employees.viewAll();
+                employees.viewAll()
+                    .then(() => promptUser());
                 break;
             case 'Add Department':
                 inquirer.prompt({
@@ -55,7 +59,8 @@ const promptUser = () => {
                     name: 'name',
                     message: "What is the department's name?"
                 }).then(({ name }) => {
-                    departments.add([name]);
+                    departments.add([name])
+                        .then(() => promptUser());
                 });                        
                 break;
             case 'Add Role':
@@ -76,18 +81,48 @@ const promptUser = () => {
                         message: "What department id does this role belong to?"
                     }
                 ]).then(({ title, salary, department_id }) => {
-                    roles.add([title, salary, department_id]);
+                    roles.add([title, salary, department_id])
+                        .then(() => promptUser());
                 });               
                 break;
             case 'Add Employee':
-
+                let employeeNames = employees.getNames();
+                console.log(employeeNames);
+                
+                inquirer.prompt([
+                    {
+                        type: 'text',
+                        name: 'first_name',
+                        message: "What is the employee's first name?"
+                    },
+                    {
+                        type: 'text',
+                        name: 'last_name',
+                        message: "What is the employee's last name?"
+                    },
+                    // {
+                    //     type: 'text',
+                    //     name: 'title',
+                    //     message: "What is this employee's title?",
+                    //     choices: []
+                    //     // role array
+                    // },
+                    {
+                        type: 'list',
+                        name: 'title',
+                        message: "Who is this employee's manager?",
+                        choices: employeeNames
+                    }
+                ]).then(() => promptUser()); 
                 break;
             case 'Update Employee Role':
                 
                 break;
+            case 'Quit Application':
+                db.end();
+                break;
         }
     });
-    // promptUser();
 };
 
 begin();
--- queries.js
+++ queries.js
@@ -1,40 +1,58 @@
 const db = require('./db/connection');
 const promptUser = require('./index')
 
-class Database {
+class Table {
     constructor(sqlQuery, sqlPost) {
         this.sqlQuery = sqlQuery;
         this.sqlPost = sqlPost;
     }
 
     viewAll() {
-        db.promise().query(this.sqlQuery)
+        return db.promise().query(this.sqlQuery)
             .then( ([rows, fields]) => {
                 console.log('');
-                console.table(rows);
+                return console.table(rows);
             })
-            .catch(console.log)
-            .then( () => db.end());
+            .catch(console.log);
     }
 
     add(params) {
-        db.promise().query(this.sqlPost, params)
+        return db.promise().query(this.sqlPost, params)
             .then( ([rows, fields]) => {
                 console.log('');
                 console.log(`${params[0]} was successfully added.`)
+                console.log('');
             })
-            .catch(console.log)
-            .then( () => db.end());
+
+            .catch(console.log);
     }
 }
 
-const departments = new Database(`SELECT
+class EmployeesTable extends Table {
+    constructor(sqlQuery, sqlPost) {
+        super(sqlQuery, sqlPost);
+    }
+
+    getNames() {
+        return db.promise().query(this.sqlQuery)
+            .then( ([rows, fields]) => {
+                let employeeNames = rows.map(employee => ({
+                    name: employee.first_name + ' ' + employee.last_name,
+                    value: employee.id
+                }))
+                return employeeNames;
+            })
+    };
+};
+
+
+const departments = new Table(`SELECT
     departments.name,
     departments.id
     FROM departments;`,
     `INSERT INTO departments (name) VALUES (?)`);
 
-const roles = new Database(`SELECT
+const roles = new Table(`SELECT
     roles.title AS 'job title',
     roles.id,
     departments.name AS department,
@@ -43,7 +61,7 @@ const roles = new Database(`SELECT
     LEFT JOIN departments ON roles.department_id = departments.id;`,
     `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`);
 
-const employees = new Database(`SELECT
+const employees = new EmployeesTable(`SELECT
     employees.id,
     employees.first_name,
     employees.last_name,
@@ -54,6 +72,7 @@ const employees = new Database(`SELECT
     FROM employees
     LEFT JOIN roles ON employees.role_id = roles.id
     LEFT JOIN departments ON roles.department_id = departments.id
-    LEFT JOIN employees as managers ON employees.manager_id = managers.id;`);
+    LEFT JOIN employees as managers ON employees.manager_id = managers.id;`,
+    `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`);
 
 module.exports = { departments, roles, employees };
